3dfd1331ed1716dfa78107ced3cbe26d9eefbbc1
arturovt <arthurandrosovich@gmail.com>
2022-03-31 08:08:55 +0300

    Upgrade to Angular 13

--- browser-test/test.js
+++ browser-test/test.js
@@ -1,9 +1,37 @@
-describe("@esm-bundle/autopublish-template", () => {
-  it("can load the ESM bundle", () => {
-    return import("/base/esm/index.js");
+describe("@esm-bundle/angular__service-worker", () => {
+  describe("@angular/service-worker", () => {
+    ["es2015", "es2020"].forEach((ecma) => {
+      it(`can load the System.register ${ecma} bundle`, async () => {
+        const m = await System.import(
+          `/base/system/${ecma}/ivy/angular-service-worker.js`
+        );
+        expect(m.ServiceWorkerModule).toBeDefined();
+      });
+
+      it(`can load the System.register ${ecma} prod bundle`, async () => {
+        const m = await System.import(
+          `/base/system/${ecma}/ivy/angular-service-worker.min.js`
+        );
+        expect(m.ServiceWorkerModule).toBeDefined();
+      });
+    });
   });
 
-  it("can load the System.register bundle", () => {
-    return System.import("/base/system/index.js");
+  describe("@angular/service-worker/config", () => {
+    ["es2015", "es2020"].forEach((ecma) => {
+      it(`can load the System.register ${ecma} bundle`, async () => {
+        const m = await System.import(
+          `/base/system/${ecma}/ivy/angular-config.js`
+        );
+        expect(m.Generator).toBeDefined();
+      });
+
+      it(`can load the System.register ${ecma} prod bundle`, async () => {
+        const m = await System.import(
+          `/base/system/${ecma}/ivy/angular-config.min.js`
+        );
+        expect(m.Generator).toBeDefined();
+      });
+    });
   });
 });
